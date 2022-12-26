5912ef32aa3b127b332cebf991e54fe26abaf951
wangshiyuan <wangshiyuan@zhito.com>
2022-12-26 09:22:10 +0800

    change map api version

--- src/views/main/vehicle-monitor/map.tsx
+++ src/views/main/vehicle-monitor/map.tsx
@@ -8,17 +8,17 @@ function Map() {
     /**
      * 全局参数设置
      */
-    minemap.domainUrl = 'https://minemap.minedata.cn';
-    minemap.dataDomainUrl = 'https://minemap.minedata.cn';
-    minemap.serverDomainUrl = 'https://sd-data.minedata.cn';
-    minemap.spriteUrl = 'https://minemap.minedata.cn/minemapapi/v3.4.0/sprite/sprite';
-    minemap.serviceUrl = 'https://service.minedata.cn/service';
-    minemap.key = "ca78282dfff148e3be5390ce027068f5";
-    minemap.solution = 11001;
+     minemap.domainUrl = "//minedata.cn";
+     minemap.dataDomainUrl = "//minedata.cn";
+     minemap.serverDomainUrl = "//minedata.cn";
+     minemap.spriteUrl = "//minedata.cn/minemapapi/v2.1.0/sprite/sprite";
+     minemap.serviceUrl = "//minedata.cn/service/";
+    minemap.key = "16be596e00c44c86bb1569cb53424dc9";
+    minemap.solution = 12612;
 
     const map = new minemap.Map({
       container: "map",
-      style: "https://service.minedata.cn/map/solu/style/11001",
+      style: "//minedata.cn/service/solu/style/id/12612",
       center: [120.647445, 31.416957],
       zoom: 12,
       pitch: 0,
