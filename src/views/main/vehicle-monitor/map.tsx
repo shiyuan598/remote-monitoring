45b85150a6c0d56db491a0a245846f584a33d84d
wangshiyuan <wangshiyuan@zhito.com>
2022-12-26 10:27:42 +0800

    use 2d map

--- src/views/main/vehicle-monitor/map.tsx
+++ src/views/main/vehicle-monitor/map.tsx
@@ -1,105 +1,105 @@
 import React, { useState, useEffect } from "react";
 import { vehicle as vehicleApi } from "../../../api";
-import { message } from 'antd';
+import { message } from "antd";
 
 function Map() {
-  const [map, setMap] = useState(null);
-  useEffect(() => {
-    /**
-     * 全局参数设置
-     */
-     minemap.domainUrl = "//minedata.cn";
-     minemap.dataDomainUrl = "//minedata.cn";
-     minemap.serverDomainUrl = "//minedata.cn";
-     minemap.spriteUrl = "//minedata.cn/minemapapi/v2.1.0/sprite/sprite";
-     minemap.serviceUrl = "//minedata.cn/service/";
-    minemap.key = "16be596e00c44c86bb1569cb53424dc9";
-    minemap.solution = 12612;
+    const [map, setMap] = useState(null);
+    useEffect(() => {
+        /**
+         * 全局参数设置
+         */
+        minemap.domainUrl = "https://minemap.minedata.cn";
+        minemap.dataDomainUrl = "https://minemap.minedata.cn";
+        minemap.serverDomainUrl = "https://sd-data.minedata.cn";
+        minemap.spriteUrl = "https://minemap.minedata.cn/minemapapi/v2.1.1/sprite/sprite";
+        minemap.serviceUrl = "https://service.minedata.cn/service";
+        minemap.key = "ca78282dfff148e3be5390ce027068f5";
+        minemap.solution = 11001;
 
-    const map = new minemap.Map({
-      container: "map",
-      style: "//minedata.cn/service/solu/style/id/12612",
-      center: [120.647445, 31.416957],
-      zoom: 12,
-      pitch: 0,
-      maxZoom: 17,
-      minZoom: 3,
-      projection: "MERCATOR",
-      logoControl: false
-    });
-    map.addControl(new minemap.Navigation(), 'bottom-right');
-    map.addControl(new minemap.Fullscreen(), 'top-right');
-    map.addControl(new minemap.Scale(), 'bottom-left');
+        const map = new minemap.Map({
+            container: "map",
+            style: "https://service.minedata.cn/map/solu/style/11001",
+            center: [120.647445, 31.416957],
+            zoom: 12,
+            pitch: 0,
+            maxZoom: 17,
+            minZoom: 3,
+            projection: "MERCATOR",
+            logoControl: false
+        });
+        map.addControl(new minemap.Navigation(), "bottom-right");
+        map.addControl(new minemap.Fullscreen(), "top-right");
+        map.addControl(new minemap.Scale(), "bottom-left");
 
-    map.on("click", (e: any) => {
-      console.info([e.lngLat.lng, e.lngLat.lat]);
-    });
-    setMap(map);
-  }, []);
+        map.on("click", (e: any) => {
+            console.info([e.lngLat.lng, e.lngLat.lat]);
+        });
+        setMap(map);
+    }, []);
 
-  const [vehicles, setVehicles] = useState([]);
-  useEffect(() => {
-    let timer: any = null;
-    const getVehicles = () => {
-      let atHomePage = window.location.href.includes("/desktop/home");
-      if (!atHomePage){
-        timer = setTimeout(getVehicles, 1000);
-        return;
-      }
-      vehicleApi.getVehiclesPosition().then(v => {
-        if (v.code === 0) {
-          setVehicles(v.data);
-          timer = setTimeout(getVehicles, 1000);
-        } else {
-          message.error("出错了");
-        }
-      });
-    };
+    const [vehicles, setVehicles] = useState([]);
+    useEffect(() => {
+        let timer: any = null;
+        const getVehicles = () => {
+            let atHomePage = window.location.href.includes("/desktop/home");
+            if (!atHomePage) {
+                timer = setTimeout(getVehicles, 1000);
+                return;
+            }
+            vehicleApi.getVehiclesPosition().then(v => {
+                if (v.code === 0) {
+                    setVehicles(v.data);
+                    timer = setTimeout(getVehicles, 1000);
+                } else {
+                    message.error("出错了");
+                }
+            });
+        };
 
-    getVehicles();
+        getVehicles();
 
-    return () => {
-      clearTimeout(timer);
-    }
-  }, []);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
 
-  const [popups, setPopups] = useState([]);
-  const [makers, setMakers] = useState([]);
-  useEffect(() => {
-    if (map) {
-      // 删除所有markers和popups
-      makers.forEach((item: any) => item.remove());
-      popups.forEach((item: any) => item.remove());
+    const [popups, setPopups] = useState([]);
+    const [makers, setMakers] = useState([]);
+    useEffect(() => {
+        if (map) {
+            // 删除所有markers和popups
+            makers.forEach((item: any) => item.remove());
+            popups.forEach((item: any) => item.remove());
 
-      let _popups: any = [];
-      let _markers: any = [];
-      vehicles.forEach((item: any) => {
-
-        let popup = new minemap.Popup({ closeOnClick: true, closeButton: false, offset: [0, -20] })
-          .setLngLat([item.lon, item.lat])
-          .setHTML(`
+            let _popups: any = [];
+            let _markers: any = [];
+            vehicles.forEach((item: any) => {
+                let popup = new minemap.Popup({ closeOnClick: true, closeButton: false, offset: [0, -20] })
+                    .setLngLat([item.lon, item.lat])
+                    .setHTML(
+                        `
           <h4>${item.vehicleNo}</h4>
-        `).addTo(map);
-        _popups.push(popup);
+        `
+                    )
+                    .addTo(map);
+                _popups.push(popup);
 
-        let marker = new minemap.Marker({
-          color: '#f30',
-          rotation: 0,
-          pitchAlignment: 'map',
-          rotationAlignment: 'map',
-          scale: 0.6
-        }).setLngLat([item.lon, item.lat])
-          .addTo(map);
-        _markers.push(marker);
-      });
-      setPopups(_popups);
-      setMakers(_markers);
-    }
-  }, [map, vehicles]);
-  return (
-    <div id="map">
-    </div>
-  );
+                let marker = new minemap.Marker({
+                    color: "#f30",
+                    rotation: 0,
+                    pitchAlignment: "map",
+                    rotationAlignment: "map",
+                    scale: 0.6
+                })
+                    .setLngLat([item.lon, item.lat])
+                    .addTo(map);
+                _markers.push(marker);
+            });
+            setPopups(_popups);
+            setMakers(_markers);
+        }
+    }, [map, vehicles]);
+    return <div id="map"></div>;
 }
 
 export default React.memo(Map);
